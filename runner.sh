15a77dcd2d48a1826798b6c17a8635ab5c709cd1
Deep Chordia <73215996+DeepC004@users.noreply.github.com>
2023-04-27 13:26:32 +0530

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,3 +1,3 @@
-g++ src/segment-least-squares.cpp dbg.hh -o segment -std=c++17
+g++ src/segment-least-squares.cpp src/dbg.hh -o segment -std=c++17
 ./segment < $1 > out.txt
 python3 ./utils/vis.py < out.txt
