73275633c2f62656d530f1f1dd788e3eede4eac4
Ashwin Pugalia <98446038+Ashwin-1709@users.noreply.github.com>
2023-04-28 21:02:20 +0530

    Update Ford-Fulkerson-Flow.cpp

--- src/Ford-Fulkerson-Flow.cpp
+++ src/Ford-Fulkerson-Flow.cpp
@@ -7,17 +7,18 @@ int32_t main() {
     int N , M;
     cin >> N >> M;
     vector<vector<int64_t>>adj(N + 1), residualCapacity(N + 1 , vector<int64_t>(N + 1));
-
+    set<array<int64_t , 2>>forwardEdgeSet;
     for(int i = 0 ; i < M ; i++) {
-        int64_t source, dest, capacity;
+        int64_t source, dest, capacity; 
         cin >> source >> dest >> capacity;
+        forwardEdgeSet.insert({source, dest});
         adj[source].push_back(dest);
         adj[dest].push_back(source);
         residualCapacity[source][dest] += capacity;
     }
 
     int64_t flow = 0;
-    int64_t s, t; cin >> s >> t;
+    int64_t s, t; cin >> s >> t; 
 
-    cout << findFlow(s, t, residualCapacity, adj) << '\n';
+    cout << findFlow(s, t, residualCapacity, adj, forwardEdgeSet) << '\n';
 }
